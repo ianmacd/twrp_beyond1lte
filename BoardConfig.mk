86a6596081fc22b4ad5cefc51bff16aff44c6aa2
Ian Macdonald <ian@caliban.org>
2019-08-18 12:23:44 +0200

    Version 3.3.1-6_ianmacd.
    Change-Id: I95657de33cded2c6316ae3b90a45e9da6b6e4301

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Version 3.3.1-5_ianmacd.
+# Version 3.3.1-6_ianmacd.
 
 # Architecture
 TARGET_ARCH := arm64
@@ -86,4 +86,4 @@ TW_NO_LEGACY_PROPS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := device/samsung/beyond1lte/prebuilt/recovery_dtbo
-PLATFORM_SECURITY_PATCH := 2019-07-31
+PLATFORM_SECURITY_PATCH := 2019-08-18
